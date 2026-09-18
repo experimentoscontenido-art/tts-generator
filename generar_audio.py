7de51e018b233dc978f46a4922ed520934dee512
experimentoscontenido-art <experimentoscontenido@gmail.com>
2026-09-18 00:10:17 -0300

    Update generar_audio.py

--- generar_audio.py
+++ generar_audio.py
@@ -1,12 +1,19 @@
 import edge_tts
 import asyncio
 import sys
+import os
 
 texto = sys.argv[1]
+id_ejecucion = sys.argv[2]
+indice = sys.argv[3]
 voz = "es-AR-ElenaNeural"
 
+carpeta = id_ejecucion
+archivo_salida = f"{carpeta}/escena_{indice}.mp3"
+
 async def main():
+    os.makedirs(carpeta, exist_ok=True)
     communicate = edge_tts.Communicate(texto, voz)
-    await communicate.save("output.mp3")
+    await communicate.save(archivo_salida)
 
 asyncio.run(main())
